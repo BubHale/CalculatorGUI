dd1a2ff5403cf3c9bd11d30f56e3358cf6217d38
BubHale <101237864+BubHale@users.noreply.github.com>
2022-03-08 23:48:44 -0500

    Add files via upload

---
+++ MyForm.cpp
@@ -0,0 +1,18 @@
+#include "MyForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+//[Stathread]
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	CalculatorGUI::MyForm form;
+
+	Application::Run(% form);
+
+}
